26466cc89c90a2d7860047b517e8b0b633145210
Kevin Boston <kbos22@gmail.com>
2020-07-27 14:47:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,9 +3,14 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
-	get '/hello'
+	get '/hello' do
 	  erb :hello
 	end
-
+	get '/goodbye' do
+	  erb :goodbye
+	end
+	get '/date' do 
+	  erb :date 
+	end
 
 end
